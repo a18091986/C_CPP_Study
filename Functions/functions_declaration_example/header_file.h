dfdbbd5c2e22fe2819d65610131cf2b46b0d9b57
behappyman <a18091986@gmail.com>
2022-10-26 14:36:00 +0300

    functions_declaration_example added

---
+++ Functions/functions_declaration_example/header_file.h
@@ -0,0 +1,6 @@
+#ifndef HEADER_FILE_H // ifndef == if not defined
+#define HEADER_FILE_H
+
+void advancedArraisMain(void);
+
+#endif /*HEADER_FILE_H*/
---
+++ Functions/functions_declaration_example/outer_function.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+void advancedArraisMain(void) {
+    printf("multi-dimmensional Arrays!\n");
+}
