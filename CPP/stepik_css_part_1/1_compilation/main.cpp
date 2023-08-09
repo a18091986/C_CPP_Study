72d3981addc92e2f6ab56d3d1f5db0242c196e9b
behappyman <behappyman@kali.monoblok.banja>
2023-08-09 20:26:13 +0300

    CPP start

---
+++ CPP/stepik_css_part_1/1_compilation/main.cpp
@@ -0,0 +1,7 @@
+#include "square.hpp"
+#include <stdio.h>
+
+int main() {
+    printf("%d", square(5));
+    return 0;
+}
---
+++ CPP/stepik_css_part_1/1_compilation/main_preprocess.cpp
@@ -0,0 +1,17 @@
+# 0 "main.cpp"
+# 0 "<built-in>"
+# 0 "<command-line>"
+# 1 "/usr/include/stdc-predef.h" 1 3 4
+# 0 "<command-line>" 2
+# 1 "main.cpp"
+# 1 "square.hpp" 1
+
+
+
+int square(int x);
+# 2 "main.cpp" 2
+
+int main() {
+    square(5);
+    return 0;
+}
